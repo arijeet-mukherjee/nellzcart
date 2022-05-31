a28f855460863d061980b1c6ab09197ea2aea7d2
emmyari <39984190+emmyari@users.noreply.github.com>
2022-05-31 19:57:17 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Template from "./template/Template";
+import ProductDetail from "./products/detail/ProductDetail";
+import { Switch, Route } from "react-router-dom";
+import Landing from "./landing/Landing";
+import ProductList from "./products/ProductList";
+import Category from "./categories/categories";
+
+function App() {
+  return (
+    <Template>
+      <Switch>
+        <Route path="/category/:slug" exact>
+          <Category />
+        </Route>
+        <Route path="/products" exact>
+          <ProductList />
+        </Route>
+        <Route path="/products/:slug">
+          <ProductDetail />
+        </Route>
+        <Route path="/" exact>
+          <Landing />
+        </Route>
+      </Switch>
+    </Template>
+  );
+}
+
+export default App;
