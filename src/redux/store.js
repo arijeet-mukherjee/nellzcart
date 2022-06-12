bdf69d938fa35a2b25a8ea0b1e8df8e3689df228
emmyari <39984190+emmyari@users.noreply.github.com>
2022-06-12 15:53:00 +0530

    ..

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,16 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import { persistStore,persistReducer } from "redux-persist";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
-import storage from 'redux-persist/lib/storage'
 
 const middlewares = [];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const persistConfig = {
-  key: 'root',
-  storage,
-}
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
 
 
