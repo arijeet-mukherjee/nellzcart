ff268f4b553f8f0e278b6501ef09b171d0d08f05
emmyari <39984190+emmyari@users.noreply.github.com>
2022-06-12 15:31:53 +0530

    redux-persit-solved

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,22 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import { persistStore } from "redux-persist";
+import { persistStore,persistReducer } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
+import storage from 'redux-persist/lib/storage'
 
 const middlewares = [];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
-
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+export const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(...middlewares)));
 
 
 export const persistor = persistStore(store);
